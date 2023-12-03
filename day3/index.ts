15220bb7c3cc5bbe5ab7069d60c1ddf527d67608
alexia sousa <alexialsousa@hotmail.com>
2023-12-03 13:30:45 -0500

    day 3 part 1 & 2

---
+++ day3/index.ts
@@ -0,0 +1,216 @@
+import { readFileSync } from 'fs';
+
+function part1(){
+    const file = readFileSync('day3/input.txt', 'utf-8');
+    const lines: string[] = file.split('\n');
+    const lineLength = lines[0].length;
+    const fileLength = lines.length;
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,<>\/?~]/;
+    let foundNumbersLocations: Map<number, number> = new Map();
+    let total = 0;
+
+    for (var i = 0; i < fileLength; i++) {
+        for (var j = 0; j < lineLength; j++) {
+            if (specialChars.test(lines[i][j])) {
+                // check if character above, below, left, right or diagonal is a number
+
+                //above
+                if(lines[i-1][j].match(/\d/)){
+                    findFullNumber(i-1, j);
+                }
+
+                //below
+                if(lines[i+1][j].match(/\d/)){
+                    findFullNumber(i+1, j);
+                }
+
+                //left
+                if(lines[i][j-1].match(/\d/)){
+                    findFullNumber(i, j-1);
+                }
+
+                //right
+                if(lines[i][j+1].match(/\d/)){
+                    findFullNumber(i, j+1);
+                }
+
+                //diagonal up left
+                if(lines[i-1][j-1].match(/\d/)){
+                    findFullNumber(i-1, j-1);
+                }
+
+                //diagonal up right
+                if(lines[i-1][j+1].match(/\d/)){
+                    findFullNumber(i-1, j+1);
+                }
+
+                //diagonal down left
+                if(lines[i+1][j-1].match(/\d/)){
+                    findFullNumber(i+1, j-1);
+                }
+
+                //diagonal down right
+                if(lines[i+1][j+1].match(/\d/)){
+                    findFullNumber(i+1, j+1);
+                }
+
+            }
+        }
+    }
+
+    function findFullNumber(x: number, y: number){
+        let counterLeft = y;
+        let counterRight = y + 1;
+        let num: string = "";
+        while (counterLeft >= 0 && lines[x][counterLeft].match(/\d/) ) {
+            num = lines[x][counterLeft] + num;
+            counterLeft--;
+        }
+
+        while (counterRight < lineLength && lines[x][counterRight].match(/\d/)) {
+            num = num + lines[x][counterRight];
+            counterRight++;
+        }
+        
+        if(foundNumbersLocations.has(x) && foundNumbersLocations.get(x) == (counterLeft + 1)){
+            console.log('Already found this number');
+        }
+        else {
+            foundNumbersLocations.set(x, counterLeft + 1);
+            total += parseInt(num);
+        }
+        
+    }
+
+    console.log(total);
+}
+
+function part2(){
+    const file = readFileSync('day3/input.txt', 'utf-8');
+    const lines: string[] = file.split('\n');
+    const lineLength = lines[0].length;
+    const fileLength = lines.length;
+    const gear: string = '*';
+    let total = 0;
+    const foundNumbersLocations: Map<number, number> = new Map();
+
+    for (var i = 0; i < fileLength; i++) {
+        for (var j = 0; j < lineLength; j++) {
+            if (lines[i][j] == gear) {
+                let numCounter = 0;
+                let product = 1;
+                // check if character above, below, left, right or diagonal is a number
+
+                //above
+                if(lines[i-1][j].match(/\d/)){
+                    const n = findFullNumber(i-1, j)
+                    if(n != 0){
+                        numCounter++;
+                        product *= n;
+                    }
+                }
+
+                //below
+                if(lines[i+1][j].match(/\d/)){
+                    const n = findFullNumber(i+1, j);
+                    if (n != 0){
+                        numCounter++;
+                        product *= n;
+                    }
+                }
+
+                //left
+                if(lines[i][j-1].match(/\d/)){
+                    const n = findFullNumber(i, j-1);
+                    if (n != 0){
+                        numCounter++;
+                        product *= n;
+                    }
+                }
+
+                //right
+                if(lines[i][j+1].match(/\d/)){
+                    const n = findFullNumber(i, j+1);
+                    if (n != 0){
+                        numCounter++;
+                        product *= n;
+                    }
+                }
+
+                //diagonal up left
+                if(lines[i-1][j-1].match(/\d/)){
+                    const n = findFullNumber(i-1, j-1);
+                    if (n != 0){
+                        numCounter++;
+                        product *= n;
+                    }
+                }
+
+                //diagonal up right
+                if(lines[i-1][j+1].match(/\d/)){
+                    const n = findFullNumber(i-1, j+1);
+                    if (n!= 0){
+                        numCounter++;
+                        product *= n;
+                    }
+                }
+
+                //diagonal down left
+                if(lines[i+1][j-1].match(/\d/)){
+                    const n = findFullNumber(i+1, j-1);
+                    if(n != 0){
+                        numCounter++;
+                        product *= n;
+                    }
+                }
+
+                //diagonal down right
+                if(lines[i+1][j+1].match(/\d/)){
+                    const n = findFullNumber(i+1, j+1);
+                    if (n != 0){
+                        numCounter++;
+                        product *= n;
+                    }
+                }
+
+                if(numCounter == 2){
+                    total += product;
+                }
+
+            }
+        }
+    }
+
+    function findFullNumber(x: number, y: number){
+        let counterLeft = y;
+        let counterRight = y + 1;
+        let num: string = "";
+
+        while (counterLeft >= 0 && lines[x][counterLeft].match(/\d/) ) {
+            num = lines[x][counterLeft] + num;
+            counterLeft--;
+        }
+
+        while (counterRight < lineLength && lines[x][counterRight].match(/\d/)) {
+            num = num + lines[x][counterRight];
+            counterRight++;
+        }
+        
+        
+        if(foundNumbersLocations.has(x) && foundNumbersLocations.get(x) == (counterLeft + 1)){
+            return 0;
+        }
+        else {
+            foundNumbersLocations.set(x, counterLeft + 1);
+            return parseInt(num);
+        }
+        
+    }
+
+    console.log(total);
+
+}
+
+
+//part1();
+part2();
