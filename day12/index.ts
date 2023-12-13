1c903818014a23ef02730d06d860296ffc30c01e
alexia sousa <alexialsousa@hotmail.com>
2023-12-13 10:17:35 -0500

    day 12 part 2 - never finishes

--- day12/index.ts
+++ day12/index.ts
@@ -9,11 +9,12 @@ function part1(){
     let sum: number = 0;
 
     lines.forEach(line => {
+        
         const numOfDamaged: number[] = line.split(' ')[1].trim().split(',').map((num) => parseInt(num));
-        var validSymbols = ['#', '.'];
         const symbols: string[] = line.split(' ')[0].trim().split('');
-
+        console.log('Starting line: ' + symbols.join(''));
         sum += generate_and_count(symbols, numOfDamaged, 0);
+        console.log('Sum: ' + sum);
     });
     console.log(sum);
 }
@@ -50,9 +51,24 @@ function generate_and_count(symbols: string[], numOfDamaged: number[], index: nu
 }
 
 function isValidCombo(newLine: string, numbers: number[]): boolean {
-   
+
     let valid: boolean = true;
     let index = 0;
+    let count = 0;
+
+    // check if at least numbers amount of # is present
+    newLine.split('').forEach(symbol => {
+        if(symbol === '#') count++;
+    });
+
+    // sum of all numbers in numbers
+    let s = 0;
+    numbers.forEach(num => {
+        s += num;
+    });
+
+    if(count !== s) return false;
+
     numbers.forEach(num => {
         if (!valid) return;
 
@@ -79,14 +95,6 @@ function isValidCombo(newLine: string, numbers: number[]): boolean {
             index = i + num;
     });
 
-    if(index < newLine.length){
-        for(let i = index; i < newLine.length; i++){
-            if(newLine.charAt(i) === '#'){
-                valid = false;
-            }
-        }
-    }
-
     return valid;
 }
 
@@ -98,7 +106,39 @@ function findFirstDamaged(line: string, index: number){
 }
 
 function part2(){
+    const lines = file.split('\n');
+    let sum: number = 0;
 
+    lines.forEach(line => {
+        const numOfDamagedPreExpansion: number[] = line.split(' ')[1].trim().split(',').map((num) => parseInt(num));
+        const symbolsPreExpansion: string[] = line.split(' ')[0].trim().split('');
+
+        // after expansion
+        const numOfDamaged: number[] = [];
+        const symbols: string[] = [];
+
+        // for each number in numOfDamagedPreExpansion add it to numOfDamaged 5 times
+        for(let i = 0; i < 5; i++){
+            numOfDamagedPreExpansion.forEach(num => {
+                numOfDamaged.push(num);
+            });
+        }
+
+
+        // for each symbol in symbolsPreExpansion add it to symbols 5 times
+        for(let i = 0; i < 5; i++){
+            symbolsPreExpansion.forEach(symbol => {
+                symbols.push(symbol);
+            });
+            if(i != 4) symbols.push('?');
+        }
+
+
+        console.log('Starting line: ' + symbols.join(''));
+        sum += generate_and_count(symbols, numOfDamaged, 0);
+        console.log('Sum: ' + sum);
+    });
+    console.log(sum);
 }
 
 part1();
