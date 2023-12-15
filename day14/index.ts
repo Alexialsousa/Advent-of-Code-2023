fbf277d8d9d34e2c2c2339d6b367f84044d920f1
alexia sousa <alexialsousa@hotmail.com>
2023-12-14 22:52:10 -0500

    day 14 part 2

--- day14/index.ts
+++ day14/index.ts
@@ -6,12 +6,6 @@ const file = readFileSync('day14/input.txt', 'utf-8');
 function part1(){
 
     const map: string[][] = file.split('\r\n').map(row => row.split(''));
-    const positions: Map<number, number[]> = new Map(); // where row is key and value is array of indexes of 0s
-    const length = map.length;
-    let sum: number = 0;
-    // go row by row and look at each column
-    // if you find a 0, take its row and minus 1, make sure the new row has nothing in that index, continue until you find another 0 or a # (or end of map) then stay back
-    // once 0 is moved, put its new row into positions map as key with its horizontal index in value
 
     map.forEach((row, rowIndex) => {
         row.forEach((col, colIndex) => {
@@ -25,19 +19,123 @@ function part1(){
                     map[newRow + 1][colIndex] = 'O';
                     map[rowIndex][colIndex] = '.';
                 }
-
-                positions.set(newRow + 1, positions.get(newRow + 1) ? [...positions.get(newRow + 1)!, colIndex] : [colIndex]);
             }
         });
     });
 
-    
-
-    positions.forEach((value, key) => {
-        sum+=(length-key)*value.length;
-    });
-
-    console.log(sum);
+    console.log(calculateLoad(map));
 }
 
-part1();
+function part2(){
+    const map: string[][] = file.split('\r\n').map(row => row.split(''));
+
+    function moveNorth(m: string[][]): string[][]{
+        m.forEach((row, rowIndex) => {
+            row.forEach((col, colIndex) => {
+                if(col === 'O'){
+    
+                    let newRow = rowIndex - 1;
+                    while(newRow >= 0 && m[newRow][colIndex] === '.'){
+                        newRow--;
+                    }
+                    
+                    if(m[newRow + 1][colIndex] !== 'O'){
+                        m[newRow + 1][colIndex] = 'O';
+                        m[rowIndex][colIndex] = '.';
+                    }
+                }
+            });
+        });
+        return m;
+    }
+
+    function moveWest(m: string[][]): string[][]{
+        for(let col = 0; col < m[0].length; col++){
+            for(let row = 0; row < m.length; row++){
+                if(m[row][col] === 'O'){
+                    let newCol = col - 1;
+                    while(newCol >= 0 && m[row][newCol] === '.'){
+                        newCol--;
+                    }
+                    if(m[row][newCol + 1] !== 'O'){
+                        m[row][newCol + 1] = 'O';
+                        m[row][col] = '.';
+                    }
+                }   
+            }
+        }
+        return m;
+    }
+
+    function moveSouth(m: string[][]): string[][]{
+        for(let row = m.length - 1; row >= 0; row--){
+            map[row].forEach((col, colIndex) => {
+                if(col === 'O'){
+    
+                    let newRow = row + 1;
+                    while(newRow < m.length && m[newRow][colIndex] === '.'){
+                        newRow++;
+                    }
+                   
+                    if(m[newRow - 1][colIndex] !== 'O'){
+                        m[newRow - 1][colIndex] = 'O';
+                        m[row][colIndex] = '.';
+                    }
+                }
+            });
+        };
+        return m;
+    }
+
+    function moveEast(m: string[][]): string[][]{
+        for(let col = m[0].length - 1; col >= 0 ; col--){
+            for(let row = 0; row < m.length; row++){
+                if(m[row][col] === 'O'){
+                    let newCol = col + 1;
+                    while(newCol < m[0].length && m[row][newCol] === '.'){
+                        newCol++;
+                    }
+                    if(m[row][newCol - 1] !== 'O'){
+                        m[row][newCol - 1] = 'O';
+                        m[row][col] = '.';
+                    }
+                }   
+            }
+        }
+        return m;
+    }
+    let mapNorth: string[][] = [];
+    let mapWest: string[][] = [];
+    let mapSouth: string[][] = [];
+    let mapEast: string[][] = map;
+
+    for(let i = 0; i <= (119+22); i++){
+
+        // starting at i = 119, every 22 steps the map will be the same!!
+        // since (1 000 000 000 - 120) % 22 == 0 this works
+        
+        mapNorth = moveNorth(mapEast);
+        mapWest = moveWest(mapNorth);
+        mapSouth = moveSouth(mapWest);
+        mapEast = moveEast(mapSouth);
+    }
+
+    console.log(calculateLoad(mapEast));
+
+}
+
+//part1();
+part2();
+
+
+function calculateLoad(map: string[][]){
+    let sum = 0;
+    map.forEach((row, rowIndex) => {
+        let count = 0;
+        row.forEach(col => {
+            if(col === 'O') count++;
+        });
+        sum+=(map.length-rowIndex)*count;
+    });
+    return sum;
+}
